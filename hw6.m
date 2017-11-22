ee5b914510046cc695e5483c1b085a3a7c30babc
Jiani Gao <31349292+jennygjn@users.noreply.github.com>
2017-11-21 23:46:07 -0500

    hw6

--- hw6.m
+++ hw6.m
@@ -1,58 +1,77 @@
 close all;
 clear;
+clc;
 
-%%%%%%%%READ DATA AND DO OLS REGRESSION%%%%%
+%%%%%%%%READ DATA%%%%%
 load matlab.mat;
 Y=log(wage);
-X=[ones(size(Y)),educ,exper,smsa,black,south];
-[beta,Sigma,resid,CovB,logL]=mvregress(X,Y,'varformat','full');
+X=[ones(length(Y),1),educ,exper,smsa,black,south];
+[n,k]=size(X);
 
-%%%%%%%MH APPROACH%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%ESTIMATION%%%%
+beta=inv(X'*X)*X'*Y;
+residual=Y-X*beta;
+sigma_sq=sum((residual-mean(residual)).^2)/(length(Y)-k);
+covb=inv(X'*X)*sigma_sq;
+varb=diag(covb);
+varsig=2/(length(Y)-k)*sigma_sq^2;
+var=[varb;varsig];
+beta_mh=[beta;sigma_sq];
+r=zeros(length(beta_mh),5000);
 
-beta=repmat([beta',Sigma],[1 1 2]);
-acc=zeros(2,1);
-cov=transpose(diag(CovB));
-
-%%%%%%%%flat prior%%%%%%%%%%%%%%%%%%%%%%%%%%
-for j=2:5000
-    l=L1(Y,X,beta(j-1,1:end-1,1),beta(j-1,end,1));
-    betaNew=beta(1,:,1)+mvnrnd(zeros(size(beta(1,:,1))),cov);
-    lNew=L1(Y,X,betaNew(1:end-1),betaNew(end));
-    d=lNew-l;
+%%%%%%%part 1 flat prior%%%%%
+l=normpdf(Y-X*beta,0,sqrt(sigma_sq));
+l=sum(log(l));
+acc=0;
+for i= 2:5000
+    betanew=beta_mh+mvnrnd(zeros(size(beta_mh)),diag(var))';
+    lnew=normpdf(Y-X*betanew(1:6),0,sqrt(betanew(7)));
+    lnew=sum(log(lnew));
     c=rand;
-    
-    if(d>=c)
-        beta(j,:,1)=betaNew;
-        acc(1,1)=acc(1,1)+1;%%%accept
+    if exp(lnew-l)>=c
+        acc=acc+1;
+        l=lnew;
+        beta_mh=betanew;
     else
-        beta(j,:,1)=beta(j-1,:,1);%%%%%reject
-    end
+    end;
+    r(:,i)=beta_mh;
+end
+str={'constant','educ','exper','smsa','black','south','sigma'};
+figure
+for j=1:7
+    subplot(4,2,j)
+    histogram(r(j,:))
+    title(str{j})
 end
 
-%%%%%%%%%%%using prior for education%%%%%%%%%
-for i=2:5000
-     l=L2(Y,X,beta(i-1,1:end-1,2),beta(i-1,end,2),cov);
-    betaNew=beta(i-1,:,2)+mvnrnd(zeros(size(beta(1,:,2))),cov);
-    lNew=L2(Y,X,betaNew(1:end-1),betaNew(end),cov);
-    d=lNew-l;
+%%%%using education prior%%%%
+conf=[0.035,0.085];
+a=norminv(0.975);
+b=norminv(0.025);
+edu=0.06;
+s=(conf(2)-edu)/a;
+beta_mh=[beta;sigma_sq];
+r=zeros(length(beta_mh),5000);
+l=normpdf(Y-X*beta,0,sqrt(sigma_sq));
+l=sum(log(l))+log(normpdf(beta_mh(2)-edu,0,s));
+acc=0;
+for i= 2:5000
+    betanew=beta_mh+mvnrnd(zeros(size(beta_mh)),diag(var))';
+    lnew=normpdf(Y-X*betanew(1:6),0,sqrt(betanew(7)));
+    lnew=sum(log(lnew))+log(normpdf(betanew(2)-edu,0,s));
     c=rand;
-    if(d>=c)
-        beta(i,:,2)=betaNew;
-        acc(2,1)=acc(2,1)+1;%%acc
+    if exp(lnew-l)>=c
+        acc=acc+1;
+        l=lnew;
+        beta_mh=betanew;
     else
-        beta(i,:,2)=beta(i-1,:,2);%%%rej
-    end
+    end;
+    r(:,i)=beta_mh;
 end
-
-
-%%%%%%%%%%%%plot%%%%%%%%%%%%%%%%%%%
-str={'Constant','education','black','smsa','south','experience','Sigma'};
-title={'2(a) flat prior','2(b) education'};
-for i=1:2
-figure('Name',title{i})
-  for j=1:7
-   subplot(4,2,j);
-   histogram(beta(:,j,i));
-   title(str{j});
-  end
+str={'constant','educ','exper','smsa','black','south','sigma'};
+figure
+for j=1:7
+    subplot(4,2,j)
+    histogram(r(j,:))
+    title(str{j})
 end
